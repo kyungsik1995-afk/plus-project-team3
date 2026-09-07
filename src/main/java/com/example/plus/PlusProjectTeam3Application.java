cab2b4478327a97f7173afed12dc41c8df2a7b7d
t2025-m0237 <asd@asad>
2026-09-07 16:49:06 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/example/plus/PlusProjectTeam3Application.java
@@ -0,0 +1,13 @@
+package com.example.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusProjectTeam3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlusProjectTeam3Application.class, args);
+	}
+
+}
