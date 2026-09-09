cb03b8a974b3d514a69f45a55a1343ff3cce0d0b
KANG SUNG HYUN <newdevsh0104@gmail.com>
2026-09-09 21:57:27 +0900

    feat:상품 목록 조회 api_repository 구현

--- src/main/java/com/example/plus/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/plus/domain/product/repository/ProductRepository.java
@@ -1,7 +1,26 @@
 package com.example.plus.domain.product.repository;
 
 import com.example.plus.domain.product.entity.Product;
+import com.example.plus.domain.product.entity.ProductCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("""
+            SELECT p
+            FROM Product p
+            WHERE (:category IS NULL OR p.category = :category)
+              AND (:minPrice IS NULL OR p.price >= :minPrice)
+              AND (:maxPrice IS NULL OR p.price <= :maxPrice)
+            """)
+    Page<Product> findAllByConditions(
+            @Param("category") ProductCategory category,
+            @Param("minPrice") Long minPrice,
+            @Param("maxPrice") Long maxPrice,
+            Pageable pageable
+    );
 }
