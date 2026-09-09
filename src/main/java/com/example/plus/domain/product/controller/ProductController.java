b36518482f39794289b9b1b618a6136394a74148
KANG SUNG HYUN <newdevsh0104@gmail.com>
2026-09-09 21:55:37 +0900

    feat:상품 목록 조회 api_컨트롤러 구현

--- src/main/java/com/example/plus/domain/product/controller/ProductController.java
+++ src/main/java/com/example/plus/domain/product/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.example.plus.domain.product.controller;
 
 import com.example.plus.domain.product.dto.ProductDetailResponse;
+import com.example.plus.domain.product.dto.ProductListRequest;
+import com.example.plus.domain.product.dto.ProductListResponse;
 import com.example.plus.domain.product.dto.ProductUpdateRequest;
 import com.example.plus.domain.product.service.ProductService;
 import com.example.plus.global.common.response.ApiResponse;
@@ -8,8 +10,10 @@ import com.example.plus.global.exception.ErrorCode;
 import com.example.plus.global.exception.business.BusinessException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +26,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    @GetMapping
+    public ApiResponse<Page<ProductListResponse>> getProducts(
+            @Valid @ModelAttribute ProductListRequest request,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            String message = bindingResult.getAllErrors().get(0).getDefaultMessage();
+            throw new BusinessException(ErrorCode.INVALID_REQUEST, message);
+        }
+
+        return ApiResponse.success(productService.getProducts(request));
+    }
 
     @GetMapping("/{productId}")
     public ApiResponse<ProductDetailResponse> getProductDetail(
