49b17a81fa099c50489cd5a91c36ae73e894eef5
KANG SUNG HYUN <newdevsh0104@gmail.com>
2026-09-11 08:29:19 +0900

    feat:장바구니 상품 담기 응답 api_dto 구현

--- src/main/java/com/example/plus/domain/cart/dto/CartItemResponse.java
+++ src/main/java/com/example/plus/domain/cart/dto/CartItemResponse.java
@@ -1,5 +1,7 @@
 package com.example.plus.domain.cart.dto;
 
+import com.example.plus.domain.cart.entity.CartItem;
+
 public record CartItemResponse(
         Long cartItemId,
         Long productId,
@@ -8,4 +10,18 @@ public record CartItemResponse(
         Integer quantity,
         Long itemTotalPrice
 ) {
+
+    public static CartItemResponse from(CartItem cartItem) {
+        Long productPrice = cartItem.getProduct().getPrice();
+        Integer quantity = cartItem.getQuantity();
+
+        return new CartItemResponse(
+                cartItem.getId(),
+                cartItem.getProduct().getId(),
+                cartItem.getProduct().getName(),
+                productPrice,
+                quantity,
+                productPrice * quantity
+        );
+    }
 }
