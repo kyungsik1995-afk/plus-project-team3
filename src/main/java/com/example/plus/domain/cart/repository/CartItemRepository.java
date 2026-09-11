238bd68a05a964036bb0cc28d3f9f4b7d05d840a
KANG SUNG HYUN <newdevsh0104@gmail.com>
2026-09-11 09:18:08 +0900

    feat:장바구니 조회 api_repository 구현

--- src/main/java/com/example/plus/domain/cart/repository/CartItemRepository.java
+++ src/main/java/com/example/plus/domain/cart/repository/CartItemRepository.java
@@ -3,10 +3,13 @@ package com.example.plus.domain.cart.repository;
 import com.example.plus.domain.cart.entity.Cart;
 import com.example.plus.domain.cart.entity.CartItem;
 import com.example.plus.domain.product.entity.Product;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     Optional<CartItem> findByCartAndProduct(Cart cart, Product product);
+
+    List<CartItem> findAllByCart(Cart cart);
 }
