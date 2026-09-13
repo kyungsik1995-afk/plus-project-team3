4fe9d2f481edcd2c426a627ec6313ee09beecbd8
KANG SUNG HYUN <newdevsh0104@gmail.com>
2026-09-13 22:47:19 +0900

    refactor:장바구니 도메인 기본구조1 수정

--- src/main/java/com/example/plus/domain/cart/repository/CartRepository.java
+++ src/main/java/com/example/plus/domain/cart/repository/CartRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    Optional<Cart> findByMemberId(Long memberId);
+    Optional<Cart> findByMember_Id(Long memberId);
 }
