bf1384084e853737b3bbdb62d5ba123d63d6081f
Cizett00 <dltkdalsakak@gmail.com>
2026-09-16 15:21:35 +0900

    결제 성공 시 장바구니 삭제 기능 추가 _________________

--- src/main/java/com/example/plus/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/plus/domain/product/repository/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.example.plus.domain.product.repository;
 
 import com.example.plus.domain.product.entity.Product;
+import jakarta.persistence.LockModeType;
+import org.hibernate.internal.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 }
